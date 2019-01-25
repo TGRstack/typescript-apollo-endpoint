ee0efa2d6bae57334ab193ce3260c373ff819281
Florian Mettetal <florian.mettetal@gmail.com>
2019-01-25 12:34:05 -0800

    :fire: ($ops) x-powered-by handled by Helmet

--- src/middleware/express/security.ts
+++ src/middleware/express/security.ts
@@ -3,8 +3,6 @@ import * as express from 'express'
 import * as helmet from 'helmet'
 
 export default function expressSecurity(app: express.Application) {
-  // Don't expose any software information to potential hackers.
-  app.disable('x-powered-by')
   app.use(cors())
   app.use(helmet())
 }
